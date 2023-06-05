a0a5c3649e1a88a5a94e7415b3a8202391601e92
Azzahra Arasy Bagjamawada <74136937+azzahrarasy@users.noreply.github.com>
2023-06-05 22:47:52 +0700

    Add files via upload

---
+++ tugas18.cy.js
@@ -0,0 +1,37 @@
+describe('TugasDay18', () => {
+  it('Success_Login', () => {
+    cy.visit('https://www.saucedemo.com/')
+    cy.get('[data-test="username"]').type("standard_user")
+    cy.get('[data-test="password"]').type("secret_sauce")
+    cy.get('[data-test="login-button"]').click()
+    cy.get('.app_logo').should('be.visible')
+    cy.get('.header_secondary_container').should('be.visible')
+  })
+  it('Failed_Login', () => {
+    cy.visit('https://www.saucedemo.com/')
+    cy.get('[data-test="username"]').type("salah_user")
+    cy.get('[data-test="password"]').type("secret_sauce")
+    cy.get('[data-test="login-button"]').click()
+    cy.get('[data-test="error"]').should('be.visible')
+  })
+  it('Card', () => {
+    cy.visit('https://www.saucedemo.com/')
+    cy.get('[data-test="username"]').type("standard_user")
+    cy.get('[data-test="password"]').type("secret_sauce")
+    cy.get('[data-test="login-button"]').click()
+    cy.get('.app_logo').should('be.visible')
+    cy.get('.header_secondary_container').should('be.visible')
+    cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click()
+  })
+  it('ShoppingCard', () => {
+    cy.visit('https://www.saucedemo.com/')
+    cy.get('[data-test="username"]').type("standard_user")
+    cy.get('[data-test="password"]').type("secret_sauce")
+    cy.get('[data-test="login-button"]').click()
+    cy.get('.app_logo').should('be.visible')
+    cy.get('.header_secondary_container').should('be.visible')
+    cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click()
+    cy.get('.shopping_cart_link').click()
+  })
+
+})
